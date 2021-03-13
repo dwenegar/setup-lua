4d2c74bdef58698209832c4088325d91debccff0
dwenegar <simone.livieri@gmail.com>
2021-03-14 08:39:44 +0900

    update latest versions

--- src/input-helper.ts
+++ src/input-helper.ts
@@ -5,9 +5,9 @@ import {Inputs} from './inputs';
 const LUA_VERSION_ALIASES: {[index: string]: string} = {
   '5.1': '5.1.5',
   '5.2': '5.2.4',
-  '5.3': '5.3.5',
-  '5.4': '5.4.0',
-  latest: '5.4.0'
+  '5.3': '5.3.6',
+  '5.4': '5.4.2',
+  latest: '5.4.2'
 };
 
 const LUAROCKS_VERSION_ALIASES: {[index: string]: string} = {
@@ -15,7 +15,9 @@ const LUAROCKS_VERSION_ALIASES: {[index: string]: string} = {
   '3.1': '3.1.3',
   '3.2': '3.2.1',
   '3.3': '3.3.1',
-  latest: '3.3.1'
+  '3.4': '3.4.0',
+  '3.5': '3.5.0',
+  latest: '3.5.0'
 };
 
 export function getInputs(): Inputs {
