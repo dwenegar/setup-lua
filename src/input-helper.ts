12e5c94f6469fc955ef915fa447b9e5501de700c
Simone Livieri <simone.livieri@gmail.com>
2021-04-15 20:45:59 +0900

    remove valid version checks

--- src/input-helper.ts
+++ src/input-helper.ts
@@ -3,27 +3,6 @@ import {getInput} from '@actions/core';
 import {InputNames} from './constants';
 import {Inputs} from './inputs';
 
-const VALID_LUA_VERSIONS: string[] = [
-  '5.1.4',
-  '5.1.5',
-  '5.2.0',
-  '5.2.1',
-  '5.2.2',
-  '5.2.3',
-  '5.2.4',
-  '5.3.0',
-  '5.3.1',
-  '5.3.2',
-  '5.3.3',
-  '5.3.4',
-  '5.3.5',
-  '5.3.6',
-  '5.4.0',
-  '5.4.1',
-  '5.4.2',
-  '5.4.3'
-];
-
 const LUA_VERSION_ALIASES: {[index: string]: string} = {
   '5.1': '5.1.4',
   '5.2': '5.2.4',
@@ -31,8 +10,6 @@ const LUA_VERSION_ALIASES: {[index: string]: string} = {
   '5.4': '5.4.3',
   latest: '5.4.3'
 };
-
-const VALID_LUAROCK_VERSIONS: string[] = ['3.4.0', '3.5.0', '3.6.0'];
 
 const LUAROCKS_VERSION_ALIASES: {[index: string]: string} = {
   '3.4': '3.4.0',
@@ -52,18 +29,10 @@ export function getInputs(): Inputs {
     resolvedLuaVersion = LUA_VERSION_ALIASES[luaVersion];
   }
 
-  if (!VALID_LUA_VERSIONS.includes(resolvedLuaVersion)) {
-    core.setFailed(`Invalid lua version: ${luaVersion}`);
-  }
-
   let resolvedLuarocksVersion = luarocksVersion;
   if (luarocksVersion) {
     if (LUAROCKS_VERSION_ALIASES[luarocksVersion]) {
       resolvedLuarocksVersion = LUAROCKS_VERSION_ALIASES[luarocksVersion];
-    }
-
-    if (!VALID_LUAROCK_VERSIONS.includes(resolvedLuarocksVersion)) {
-      core.setFailed(`Invalid luarock version: ${luarocksVersion}`);
     }
   }
 
